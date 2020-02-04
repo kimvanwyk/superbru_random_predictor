562b2ef83d874c527b0f75dfe9454dcea8bd78d1
Kim van Wyk <kimv@sahomeloans.com>
2020-02-04 09:20:29 +0200

    Use namedtuple names

--- superbru_random_predictor/predictor.py
+++ superbru_random_predictor/predictor.py
@@ -1,16 +1,21 @@
 from collections import namedtuple
 import random
 
-PREDICTION = namedtuple('Prediction', ('direction', 'point_difference'))
+PREDICTION = namedtuple("Prediction", ("direction", "point_difference"))
 NUM_PREDICTIONS = 7
 
 random.seed()
 
-def make_random_prediction():
-    return PREDICTION(random.choice(('left', 'right')), random.randint(1,20))
 
-def make_predictions(num_predictions = NUM_PREDICTIONS):
+def make_random_prediction():
+    return PREDICTION(random.choice(("left", "right")), random.randint(1, 20))
+
+
+def make_predictions(num_predictions=NUM_PREDICTIONS):
     return [make_random_prediction() for i in range(num_predictions)]
 
-for (n,p) in enumerate(make_predictions(),1):
-    print(f'Prediction {n:02}: {p[0]:5} by {p[1]}')
+
+for (n, p) in enumerate(make_predictions(), 1):
+    print(f"Prediction {n:02}: {p.direction:5} by {p.point_difference}")
+    print()
+
